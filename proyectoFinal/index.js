b7c4805ebffccec89da8dce6817ebfc08a443e1a
Alberto Flores <alberto.floreso94@gmail.com>
2025-12-09 15:37:47 -0600

    add proyecto final

--- proyectoFinal/index.js
+++ proyectoFinal/index.js
@@ -1,20 +1,60 @@
+const usuario = {
+  nombre: "Beto",
+  edad: 30,
+  deuda: 0
+}
+
+let pedido = []
+let costoPedido = 0
+
 const mostrarMenu = () => {
-  let productos = [
-    {
-      producto: "Hamburguesa simple",
-      costo: 20
-    },
-    {
-      producto: "Hamburguesa doble",
-      costo: 30
-    },
-    {
-      producto: "Papas fritas",
-      costo: 8
-    },
-    {
-      producto: "Refresco personal",
-      costo: 6
-    }
-  ]
+  console.log(`CÓDIGO - NOMBRE PRODUCTO - COSTO`)
+  for (const producto of productos) {
+    console.log(`${producto.codigo} - ${producto.nombre} - ${producto.costo}`)
+  }
+}
+
+const pedirProducto = cod => {
+  if (!cod || typeof cod !== "string") return "Ingrese un código valido"
+  
+  const productoEncontrado = productos.find(producto => producto.codigo === cod)
+
+  if (!productoEncontrado) return "El producto no existe"
+
+  pedido.push(productoEncontrado)
+  console.log("El producto ha sido agregado a su pedido. Su pedido es:")
+  return verPedido()
+}
+
+const verPedido = () => pedido
+
+const calcularCosto = () => {
+  let costo = 0
+  for (const producto of pedido) {
+    costo += producto.costo
+  }
+  costoPedido = costo
+  return costoPedido
+}
+
+const finalizarPedido = () => {
+  calcularCosto()
+  usuario.deuda = costoPedido
+
+  pedido = []
+  costoPedido = 0
+
+  return `${usuario.nombre}, debes pagar ${usuario.deuda} dólares.`
+}
+
+const pagarPedido = montoEntregado => {
+  if (montoEntregado < usuario.deuda) {
+    return `No te alcanza para pagar tu pedido`
+  } else if (montoEntregado === usuario.deuda) {
+    usuario.deuda = 0
+    return `Tu pedido ha sido pagado.`
+  } else {
+    usuario.deuda = 0
+    return `Tu pedido ha sido pagado y tu cambio es de ${montoEntregado - usuario.deuda}.`
+  }
 }
