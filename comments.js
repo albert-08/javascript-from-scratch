779be2d083a8b0df303a03850c99b7738a2d5074
Alberto Flores <alberto.floreso94@gmail.com>
2026-04-03 15:34:52 -0600

    comments

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Esto es un comentario de una línea
+
+/*
+Esto es un comentario de varias líneas
+que puede abarcar varias líneas
+y es útil para explicar bloques de código 
+o proporcionar información adicional
+*/
+
+/**
+ * Este es un comentario de documentación
+ * que se utiliza para describir funciones, clases o módulos
+ * y puede ser procesado por herramientas de documentación
+ * para generar documentación automática
+ */
+
+// var hola = "Saludos"
+// console.log(hola)
+// console.log("Adios")
