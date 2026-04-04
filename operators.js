03cdd0d8bde428c5f59c8d433fc7e57a5f74f67a
Alberto Flores <alberto.floreso94@gmail.com>
2026-04-04 14:47:54 -0600

    operators

---
+++ operators.js
@@ -0,0 +1,37 @@
+/**
+ * OPERADORES DE ASIGNACIÓN
+ */
+
+let alumno
+let profe = "Juan"
+
+alumno = "Beto"
+alumno = profe
+
+/**
+ * OPERADORES ARITMÉTICOS
+ */
+
+// + - * / %
+
+let suma = 5 + 3
+console.log(suma)
+
+let resta = 5 - 3
+console.log(resta)
+
+let multiplicacion = 5 * 3
+console.log(multiplicacion)
+
+let division = 5 / 3
+console.log(division)
+
+let operacion = 62 % 4
+console.log(operacion)
+
+let numero1 = 10 - 5
+let numero2 = 4 / 2
+
+let resultado = numero1 + numero2
+
+console.log(resultado)
